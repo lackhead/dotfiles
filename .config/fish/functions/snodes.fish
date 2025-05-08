a0ec8db20f8d9c6bfc7e1a5e0f3223a81c482634
Chad Lake <clake@lackhead.org>
2025-05-07 22:33:08 -0600

    added  to sjobs and snodes

--- .config/fish/functions/snodes.fish
+++ .config/fish/functions/snodes.fish
@@ -1,3 +1,3 @@
-function snodes --description 'Slurm: show node info'
-    sinfo -o '%16P %12n %.6t %.4c %.8z %.6m %12G %10l %11L' --sort=P,n
+function snodes --wraps sinfo --description 'Slurm: show node info'
+    sinfo -o '%16P %12n %.6t %.4c %.8z %.6m %12G %10l %11L' --sort=P,n $argv
 end
