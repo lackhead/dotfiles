b71b9ff3fee1f6412ddf446645e68f83e7574ef0
Chad Lake <clake@lackhead.org>
2025-06-05 14:11:16 -0600

    adjusted justification in snodes

--- .config/fish/functions/snodes.fish
+++ .config/fish/functions/snodes.fish
@@ -1,3 +1,3 @@
 function snodes --wraps sinfo --description 'Slurm: show node info'
-    sinfo -o '%16P %12n %.6t %.4c %.8z %.6m %26G %10l %11L' --sort=P,n $argv
+    sinfo -o '%16P %12n %.6t %.4c %.8z %.6m %26G %.10l %.11L' --sort=P,n $argv
 end
