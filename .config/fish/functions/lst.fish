c0eea8d347c957c0f2b98a20dfdaea16ac902343
Chad Lake <clake@lackhead.org>
2025-07-14 16:52:32 -0600

    added -a to lst

--- .config/fish/functions/lst.fish
+++ .config/fish/functions/lst.fish
@@ -2,28 +2,35 @@ function lst --description "List most recently modified things in a given direct
 
     # configuration variables
     set NUMSHOW 10
-    set LSARGS "-ltAFh"
+    set LSARGS "-ltFh"
 
     # process args
-    argparse 'h/help' 'n/num=' 'r/reverse' -- $argv
+    argparse 'a/all' 'h/help' 'n/num=' 'r/reverse' -- $argv
        or return
+    # handle -all
+    if set -q _flag_all
+        set LSARGS (printf "%s%s" $LSARGS "A")
+    end
+    # handle --help
     if set -q _flag_help
         echo "lst - list by modification time"
         echo
         echo "Arguments:"
+        echo "    -a/--all     Show hidden files as well"
         echo "    -h/--help    What you see here"
         echo "    -n/--num     How many items to show (defult: $NUMSHOW)"
         echo "    -r/--reverse Show in oldest->newest order"
         return 0
     end
+    # handle --num
     if set -q _flag_num
         set NUMSHOW $_flag_num
     end
+    # handle --reverse
     if set -q _flag_reverse
         set LSARGS (printf "%s%s" $LSARGS "r")
     end
     
-
     # if first arg is a digit, that's the number of top things to show
     if string match -qr '^\d+$' $argv[1]
         set NUMSHOW $argv[1]
