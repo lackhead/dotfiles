a96b7133c1b45f42f992222e3b6bd2652e7af32d
Chad Lake <clake@lackhead.org>
2025-07-24 13:45:39 -0600

    updated prompt for root

--- .config/fish/conf.d/prompt.fish
+++ .config/fish/conf.d/prompt.fish
@@ -9,11 +9,12 @@ end
 
 function fish_prompt --description 'Write out the prompt'
 
-    # configuration options
+    # generic options
     set -gx fish_prompt_pwd_dir_length 2
     set -gx fish_prompt_pwd_full_dirs 2
 
-    # show any error code from previous command
+    # show any error code from previous command to wrap up the previous
+    # command 
     set -l last_pipestatus $pipestatus
     set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
     set -l status_color (set_color $fish_color_status)
@@ -23,29 +24,31 @@ function fish_prompt --description 'Write out the prompt'
     echo $status_prompt
     [ -n "$status_prompt" ] && echo 
 
-    # define how things should look
+    # define how things should look in the main prompt
     set -l prompt_suffix '>'
-    set -l prompt_hostline_color $fish_color_host
     set -l prompt_hostline_user ""
-    set -l prompt_pathline_color $fish_color_end
+    set -l prompt_hostline_color $fish_color_host
+    set -l prompt_pathline_color $fish_color_cwd
+    set -q fish_color_status
+       or set -g fish_color_status red
     # root gets things a bit differently
-    if functions -q fish_is_root_user; and fish_is_root_user
+    if fish_is_root_user
         set prompt_suffix '#'
-        set prompt_hostline_color (set_color --bold $fish_color_status)
         set prompt_hostline_user "ROOT"
-        set prompt_pathline_color $fish_color_error
+        set prompt_hostline_color $fish_color_error
     end
 
-    ## DO IT
-    # hostname
+    # show hostname
     set_color $prompt_hostline_color
     printf "[%s@%s] " $prompt_hostline_user (prompt_hostname)
     set_color normal
-    # possibly git info
+
+    # show possibly git info
     set -gx __fish_git_prompt_show_informative_status 1
     set -gx __fish_git_prompt_color $fish_color_host_remote
     echo (fish_git_prompt)
-    # path and suffix 
+
+    # show path and suffix 
     set_color $prompt_pathline_color
     printf "%s %s " (prompt_pwd) $prompt_suffix
     set_color normal
